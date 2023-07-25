f4a2b0103b5552b3eacdb6490c7974a715ab15e7
FarhathPA <140139885+FarhathPA@users.noreply.github.com>
2023-07-25 22:03:21 +0530

    Add files via upload

---
+++ fb/fb.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4,
+                maxlength:6
+            },
+            sname:{
+                required:true,
+                minlength:4
+            },
+            emailAddress:{
+                required:true,
+                email:true
+            },
+            password:{
+                minlength:6
+            },
+            day:{
+                required:true
+            },
+            gender:{
+                required:true
+            }
+        },
+        messages:{
+            fname:{
+                required:"Enter First Name",
+                minlength:"Enter Atleast 4 characters"
+            }
+        }
+    })
+})
